505ff0c92c6879a0fceef9443a63a87a9ccf2f44
Vladimir <vladimir@expofp.com>
2023-11-14 20:35:41 +0300

    Release 4.2.15

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ExpoFpCrowdConnectedTarget"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.2.14"),
+    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.2.15"),
 
         .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", from: "1.6.0"),
         .package(url: "https://github.com/crowdconnected/crowdconnected-core-ios", from: "1.6.0"),
