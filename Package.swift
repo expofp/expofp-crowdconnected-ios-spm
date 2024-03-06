ac6e4238bc753fa65ad8afc461aa28885b495066
vladimir-n0691 <vladimir@expofp.com>
2024-03-06 12:50:42 +0300

    Release 4.3.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ExpoFpCrowdConnectedTarget"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.2.20"),
+    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.3.1"),
 
         .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", from: "1.6.1"),
         .package(url: "https://github.com/crowdconnected/crowdconnected-core-ios", from: "1.6.1"),
