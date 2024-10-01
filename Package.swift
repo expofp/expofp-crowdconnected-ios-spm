fccb76af5af7088e90b9cb94d53c704eff1f41fc
vladimir-n0691 <vladimir@expofp.com>
2024-10-01 19:15:36 +0300

    Release 4.5.9

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ExpoFpCrowdConnectedTarget"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.5.8"),
+    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.5.9"),
 
         //.package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", .upToNextMinor(from: "2.28.2")),
         .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", .upToNextMinor(from: "1.6.0")),
