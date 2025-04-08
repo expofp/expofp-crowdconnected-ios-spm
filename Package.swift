3c51eaabba4f3d4b946e1647aef09435e9ab9d01
vladimir-n0691 <vladimir@expofp.com>
2025-04-08 11:38:24 +0300

    Release 4.8.19

--- Package.swift
+++ Package.swift
@@ -16,11 +16,10 @@ let package = Package(
     	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.8.18"),
 
         //.package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", .upToNextMinor(from: "2.28.2")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", .upToNextMinor(from: "2.1.2")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-core-ios", .upToNextMinor(from: "2.1.2")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-core-bluetooth-ios", .upToNextMinor(from: "2.1.2")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-ips-ios", .upToNextMinor(from: "2.1.2")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-geo-ios", .upToNextMinor(from: "2.1.2")),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", .upToNextMinor(from: "1.6.6")),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-core-ios", .upToNextMinor(from: "1.6.6")),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-ips-ios", .upToNextMinor(from: "1.6.6")),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-geo-ios", .upToNextMinor(from: "1.6.6")),
 
     ],
     targets: [
@@ -33,8 +32,7 @@ let package = Package(
                 .product(name: "CrowdConnectedShared", package: "crowdconnected-shared-ios"),
                 .product(name: "CrowdConnectedCore", package: "crowdconnected-core-ios"),
 		        .product(name: "CrowdConnectedIPS", package: "crowdconnected-ips-ios"),
-                .product(name: "CrowdConnectedGeo", package: "crowdconnected-geo-ios"),
-		        .product(name: "CrowdConnectedCoreBluetooth", package: "crowdconnected-core-bluetooth-ios"),
+		        .product(name: "CrowdConnectedGeo", package: "crowdconnected-geo-ios"),
                 .target(name: "ExpoFpCrowdConnected")
             
             ], 
