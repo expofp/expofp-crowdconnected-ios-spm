3f6a9b594c6cd0867a6dacbac1dcca1c09efc74a
vladimir-n0691 <vladimir@expofp.com>
2025-05-12 20:13:04 +0300

    Release 4.8.20

--- Package.swift
+++ Package.swift
@@ -13,13 +13,12 @@ let package = Package(
             targets: ["ExpoFpCrowdConnectedTarget"]),
     ],
     dependencies: [
-    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.8.18"),
+    	.package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.8.20"),
 
-        //.package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", .upToNextMinor(from: "2.28.2")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", .upToNextMinor(from: "1.6.6")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-core-ios", .upToNextMinor(from: "1.6.6")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-ips-ios", .upToNextMinor(from: "1.6.6")),
-        .package(url: "https://github.com/crowdconnected/crowdconnected-geo-ios", .upToNextMinor(from: "1.6.6")),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios", exact: "1.6.7"),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-core-ios", exact: "1.6.7"),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-ips-ios", exact: "1.6.7"),
+        .package(url: "https://github.com/crowdconnected/crowdconnected-geo-ios", exact: "1.6.7"),
 
     ],
     targets: [
@@ -28,7 +27,6 @@ let package = Package(
             dependencies: [
                 .product(name: "ExpoFpCommon", package: "expofp-common-ios-spm"),
 
-                //.product(name: "AWSiOSSDKV2", package: "aws-sdk-ios-spm"),
                 .product(name: "CrowdConnectedShared", package: "crowdconnected-shared-ios"),
                 .product(name: "CrowdConnectedCore", package: "crowdconnected-core-ios"),
 		        .product(name: "CrowdConnectedIPS", package: "crowdconnected-ips-ios"),
